ec5a68fdac4b82e25acb3b59ace18ace171fe445
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-21 11:41:55 +0000

    feat(vue-router): Crear rutas y exportar

--- src/routes/vue-router.js
+++ src/routes/vue-router.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Home from "../views/Home";
+import People from "../views/People";
+import PersonalData from "../views/PersonalData";
 
 Vue.use(Router);
 
 const router = new Router({
     mode: "history",
     routes: [{
-        
+        path: "/",
+        component: Home
+    },
+    {
+        path: "/people",
+        component: People
+    },
+    {
+        path: "/data",
+        component: PersonalData
     }]
 })
+
+export default router;
