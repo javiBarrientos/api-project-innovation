fb55296337c6c39c0eb7c6c8cd2f65dd99b0c8f3
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-22 10:00:53 +0000

    feat(header): Añadir botones de rutas al navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">Star Wars database</a>
+        <a class="navbar-brand" href="/">Home</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -18,6 +18,12 @@
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link" href="/people">People</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/starships">Starships</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/planets">Planets</a>
             </li>
           </ul>
         </div>
