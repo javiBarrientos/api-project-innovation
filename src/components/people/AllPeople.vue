34f94fc33e10064e3ad90b93ff47b599be4bf82c
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-22 15:32:31 +0000

    feat(allpeople): Metodo de ordenado, intento

--- src/components/people/AllPeople.vue
+++ src/components/people/AllPeople.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container">
     <h3>Personajes</h3>
+    <div class="buttonContainer">
+      <a role="button" class="btn btn-warning starships">Reset</a>
+      <a role="button" class="btn btn-warning" v-on:click="ordenarAltura"
+        >Mayor</a
+      >
+      <a role="button" class="btn btn-warning starships">Menor</a>
+    </div>
     <table class="table table-dark">
       <thead>
         <tr>
-          <th>Nombre del personaje</th>
+          <th id="name">Nombre del personaje</th>
 
-          <th>Altura</th>
+          <th id="height">Altura</th>
 
-          <th>Enlace</th>
+          <th id="link">Enlace</th>
         </tr>
       </thead>
 
@@ -49,17 +56,35 @@ export default {
       PeopleDataService.getAll()
         .then((response) => {
           this.people = response.data;
-          console.log(response.data.results);
         })
         .catch((e) => {
           console.log(e);
         });
+    },
+    ordenarAltura() {
+      function compare(a, b) {
+        if (a.height < b.height) return -1;
+        if (a.height > b.height) return 1;
+        return 0;
+      }
+
+      return this.people.sort(compare);
     },
   },
 };
 </script>
 
 <style scoped>
+.buttonContainer {
+  margin-bottom: 10px;
+  float: right;
+}
+
+.buttonContainer > a:nth-child(2) {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
 table {
   border-radius: 25px;
   background-color: #212529;
