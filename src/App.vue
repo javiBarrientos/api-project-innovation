4aadadfff302e7f39f7f68af28e436fecfea6def
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-22 13:22:28 +0000

    r

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,26 @@ export default {
   components: { Header, Footer },
 };
 </script>
+
+<style>
+@import url(https://allfont.es/allfont.css?fonts=star-jedi);
+
+#app {
+  background-image: url("./assets/background_3.jpg");
+  background-color: #cccccc;
+  height: 855px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+h1 {
+  color: yellow;
+  font-family: "Star Jedi", arial;
+  text-align: center;
+}
+
+a {
+  font-family: "Star Jedi", arial;
+}
+</style>
