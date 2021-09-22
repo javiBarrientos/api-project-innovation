993457d4dd3e85beb389379dc41e986e0840628f
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-22 13:26:47 +0000

    feat(footer): Añadir footer personalizado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div class="mainContainer">
+    <div class="mainContacts">
+      <div class="contacts">
+        <p>mail@gmail.com</p>
+        <p>+34 616 70 23 45</p>
+      </div>
+      <div class="social">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <div class="mentions">
+      <p>2018 | mentions légales</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +22,42 @@ export default {
   name: "Footer",
 };
 </script>
+
+<style scoped>
+.mainContainer {
+  padding-top: 20px;
+  margin-top: 20px;
+  color: white;
+  background-color: #212529;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+.mainContacts {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+}
+.contacts {
+  text-align: right;
+}
+.social {
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.social div {
+  height: 30px;
+  width: 30px;
+  background-color: white;
+  border-radius: 50%;
+}
+.mentions p {
+  font-size: 0.5em;
+}
+</style>
