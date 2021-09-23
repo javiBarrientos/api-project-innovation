59b81b5d62de92be54c9c3e3494037f8bad5e8bd
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-09-23 14:39:06 +0000

    refactor(personalperson): Recibir info del personaje

--- src/components/people/PersonalPerson.vue
+++ src/components/people/PersonalPerson.vue
@@ -1,11 +1,30 @@
-<template> 
-    <div>
-        
-    </div>
+<template>
+  <div>
+    <p>
+        {{personDetails.name}}
+    </p>
+  </div>
 </template>
 
 <script>
+import PeopleDataService from "../../services/people.service";
+
 export default {
   name: "PersonalPerson",
+  data() {
+    return {
+      personDetails: {},
+    };
+  },
+  mounted() {
+    this.show(this.$route.params.id);
+  },
+  methods: {
+    show(id) {
+      PeopleDataService.getPersonal(id).then((response) => {
+        this.personDetails = response.data;
+      });
+    },
+  },
 };
 </script>
